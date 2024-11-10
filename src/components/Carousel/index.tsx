91445f6df962a884e55530639d4fca80668a4306
zudva <zudva@protonmail.com>
2024-11-10 14:00:40 +0300

    Add key button clicker

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -263,7 +263,7 @@ const Carousel = () => {
 
     const handleKeyDown = (event: KeyboardEvent) => {
         console.log(event.key)
-        if (event.key === "Key.page_up") {
+        if (event.key === "Key.page_up" || event.key === "Key.page_up" || event.key === "Key.down" || event.key === "Key.ctrl_l" || event.key === "Key.enter") { {
             console.log({isStarted, card, isSelected, isStartPlayed, count, isWin: card?.isWin, currentFrame})
 
             if (!isStartPlayed) {
